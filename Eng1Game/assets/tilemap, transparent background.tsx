4707fa231919363275f270e883bd8c4a2cec2e5c
cabh500 <159497609+cabh500@users.noreply.github.com>
2024-03-14 09:17:54 +0000

    Delete Eng1Game/assets directory

--- Eng1Game/assets/tilemap, transparent background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilemap, transparent background" tilewidth="16" tileheight="16" spacing="1" tilecount="486" columns="27">
- <image source="tilemap.png" trans="000000" width="458" height="305"/>
-</tileset>
