908a7d15f2b7b04466d1c7a803f7fba0f895e3cd
cabh500 <159497609+cabh500@users.noreply.github.com>
2024-03-18 18:04:02 +0000

    Delete Eng1Game/Walls_TILESET_A4_.tsx

--- Eng1Game/Walls_TILESET_A4_.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Walls_TILESET_A4_" tilewidth="16" tileheight="16" tilecount="2160" columns="48">
- <image source="Modern_Interiors_RPG_Maker_Version/RPG_MAKER_MV/Walls_TILESET_A4_.png" trans="000000" width="768" height="720"/>
-</tileset>
