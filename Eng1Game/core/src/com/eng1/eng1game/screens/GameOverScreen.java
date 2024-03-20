71e5feccdb8108b68879618016b52411fa01c6dd
cabh500 <159497609+cabh500@users.noreply.github.com>
2024-03-20 18:42:52 +0000

    Added text code to the game over screen and fixed Grade Positioning

--- Eng1Game/core/src/com/eng1/eng1game/screens/GameOverScreen.java
+++ Eng1Game/core/src/com/eng1/eng1game/screens/GameOverScreen.java
@@ -13,10 +13,16 @@ public class GameOverScreen implements Screen {
     private Music music;
     Texture DeathGameOver;
     Texture ScoreGameOver;
+    //Image of Grades
     Texture AGrade;
     Texture BGrade;
     Texture CGrade;
     Texture FGrade;
+    //Text for grades
+    Texture AGradeText;
+    Texture BGradeText;
+    Texture FGradeText;
+
 
     public GameOverScreen(eng1Game game){
         this.game = game;
@@ -26,6 +32,9 @@ public class GameOverScreen implements Screen {
         BGrade= new Texture("grade-b.png");
         CGrade = new Texture("grade-c.png");
         FGrade = new Texture("grade-f.png");
+        AGradeText = new Texture("Grade A Text.png");
+        BGradeText = new Texture("Grade B Text.png");
+        FGradeText = new Texture("Grade F Text.png");
     }
 
     @Override
@@ -45,17 +54,21 @@ public class GameOverScreen implements Screen {
         } else {
             if (game.study == 56 && game.enjoy == 28 && game.eating == 7) {
                 game.batch.draw(ScoreGameOver, eng1Game.ORIGIN_X, eng1Game.ORIGIN_Y, eng1Game.WIDTH, eng1Game.HEIGHT);
-                game.batch.draw(AGrade, 0, 0);
+                game.batch.draw(AGrade, 0, 0, 200,200);
+                game.batch.draw(AGradeText, -200, -100,450,50);
             } else if (game.study > 56 || game.enjoy > 28 && game.eating > 5) {
                 game.batch.draw(ScoreGameOver, eng1Game.ORIGIN_X, eng1Game.ORIGIN_Y, eng1Game.WIDTH, eng1Game.HEIGHT);
-                game.batch.draw(BGrade, 0, 0);
+                game.batch.draw(BGrade, 0, 0,200,200);
+                game.batch.draw(BGradeText, -200, -100,450,50);
             }
          else if (game.study < 56 && game.study > 10 && game.enjoy < 28 && game.enjoy > 4 && game.eating <=2) {
             game.batch.draw(ScoreGameOver, eng1Game.ORIGIN_X, eng1Game.ORIGIN_Y, eng1Game.WIDTH, eng1Game.HEIGHT);
-            game.batch.draw(CGrade, 0, 0);
+            game.batch.draw(CGrade, 0, 0, 200,200);
+                game.batch.draw(FGradeText, -200, -100,450,50);
         } else {
             game.batch.draw(ScoreGameOver, eng1Game.ORIGIN_X, eng1Game.ORIGIN_Y, eng1Game.WIDTH, eng1Game.HEIGHT);
-            game.batch.draw(FGrade, 0, 0);
+            game.batch.draw(FGrade, 0, 0,200,200);
+            game.batch.draw(FGradeText, -200, -100,450,50);
         }
     }
         game.batch.end();
